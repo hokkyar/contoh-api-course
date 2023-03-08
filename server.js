1d00fc832ad7fd912beadd26ecb3d4f0dccaf3a8
hokkyar <hokkyaryasta@gmail.com>
2023-03-08 12:26:43 +0700

    first

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
+
+const data = [
+  {
+    nama: 'Web Design',
+    harga: 1000000,
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    nama: 'Python Data Analyst',
+    harga: 2000000,
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    nama: 'Ardunio',
+    harga: 500000,
+    image: 'https://picsum.photos/200/300'
+  },
+]
+
+app.use(cors())
+
+app.get('/api/course', (req, res) => {
+  res.status(200).json(data)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
